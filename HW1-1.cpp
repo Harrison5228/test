c6e5f78c7cfea5f5ba17033f3665df5f4e4a94c1
Harrison <Harrison5228@yahoo.com.tw>
2015-03-16 15:19:54 +0800

    first

---
+++ HW1-1.cpp
@@ -0,0 +1,17 @@
+/*
+學號:1033342
+姓名:哈元泰
+*/
+
+#include<iostream> //載入標頭檔
+
+using namespace std; //定義想要簡寫的名稱
+
+int main() //main function
+{
+	cout << "Alice: How are you ?\n"; //輸出字串與換行
+	cout << "Bob: Fine. Thank you.\n";
+
+	system("pause"); //讓系統暫停，檢視結果
+	return 0; //程式正常結束
+}
